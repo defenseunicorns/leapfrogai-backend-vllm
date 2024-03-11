5c4edfeff90131afc99bf5d4b7b33ecc6dbfd7a9
justinthelaw <justin.law@defenseunicorns.com>
2024-03-11 17:01:31 -0500

    int() on tensors

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ class Model:
                                            quantization=os.environ["QUANTIZATION"] or None,
                                            max_context_len_to_capture=self.backend_config.max_context_length,
                                            worker_use_ray=True,
-                                           tensor_parallel_size=os.environ["TENSOR_PARALLEL_SIZE"] or 1)
+                                           tensor_parallel_size=int(os.environ["TENSOR_PARALLEL_SIZE"]) or 1)
         self.engine = AsyncLLMEngine.from_engine_args(self.engine_args)
 
     async def iterate_outputs(self):
