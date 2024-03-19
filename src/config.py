c9700e9caf8e326f0e4a052e40a956f4bd29fe89
CollectiveUnicorn <gato.harvey@defenseunicorns.com>
2024-03-18 17:24:04 -0700

    Replaces description examples with example parameter

--- src/config.py
+++ src/config.py
@@ -7,16 +7,18 @@ from pydantic import Field
 class ConfigOptions(BaseConfig):
     quantization: Literal[None, "awq", "gptq", "squeezellm"] = Field(
         default=None,
-        description="type of quantization, for un-quantized models omit this field"
+        description="Type of quantization, for un-quantized models omit this field"
     )
     hf_hub_enable_hf_transfer: Literal["0", "1"] = Field(
         description="Option (0 - Disable, 1 - Enable) for faster transfers, tradeoff stability for faster speeds"
     )
     repo_id: str = Field(
-        description="HuggingFace repo id (e.g., TheBloke/Synthia-7B-v2.0-GPTQ)"
+        description="HuggingFace repo id",
+        examples=["TheBloke/Synthia-7B-v2.0-GPTQ", "migtissera/Synthia-MoE-v3-Mixtral-8x7B", "microsoft/phi-2"]
     )
     revision: str = Field(
-        description="The branch to use (e.g., main, gptq-4bit-64g-actorder_True, etc.)"
+        description="The model branch to use",
+        examples=["main", "gptq-4bit-64g-actorder_True"]
     )
 
 
